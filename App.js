a39e0936f42ba73ee24d447921555ce73139bc6c
Nikitha Gowlikar - LFI <ngowlikar@linkfields.com>
2025-07-04 15:08:02 +0530

    Laying the fountation Episode3

--- App.js
+++ App.js
@@ -1,9 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  { id: "Hello" },
-  "Hello World from React"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "Hello" },
+//   "Hello World from React"
+// );
+//jsx
+// const heading1=<h1>Hello World from React</h1>
+
+const Title = () => {
+  return (
+    <>
+      <h1>This is the Title component</h1>
+    </>
+  );
+};
+const SubComponent = () => {
+  return <h1>HI!!!!</h1>;
+};
+const HeadingComponent = () => {
+  return (
+    <>
+      <SubComponent />
+      {Title()}
+      <h1>Hello Heading component</h1>
+    </>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent />);
